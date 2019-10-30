a70a34a4c3d593fac048d21a0aa3bc5786acf9a8
Haohan Liu <the1nonlypanda@gmail.com>
2019-10-30 21:54:53 +1100

    Add ScriptExDee

---
+++ ScriptExDee/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace ScriptExDee
+{
+    /// <summary>
+    /// A program to blow your socks off but not blow you off.
+    /// Get ready to easily execute a bunch of scripts and shell commands
+    /// for the purpose of automating software installation, QC, and maybe
+    /// even somebody's job at some point. 
+    /// 
+    /// Fingers crossed it won't be mine though.
+    /// 
+    /// Written by Haohan Liu, but don't come askin' if shit breaks.
+    /// </summary>
+    class Program
+    {
+        // Configuration file constants
+        public static string ConfigFilename = "AppConfig.xml";
+
+        // Program PATHs
+        public static string RootPath = AppDomain.CurrentDomain.BaseDirectory;
+        public static string DriverLetter = Path.GetPathRoot(Environment.CurrentDirectory);
+        public static string SourcePath = Environment.ExpandEnvironmentVariables(DriverLetter + "FILL IN LATER");
+        public static string DestinationPath = Environment.ExpandEnvironmentVariables(DriverLetter + "FILL IN LATER");
+
+
+        /// <summary>
+        /// Main entrypoint of the program.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            TerminalTheme.ApplyTheme();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ScriptExDee/TerminalTheme.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ScriptExDee
+{
+    /// <summary>
+    /// Sets the theme of the terminal program. Mainly 'keep on top'
+    /// and terminal opacity.
+    /// 
+    /// Written by Haohan Liu
+    /// </summary>
+    static class TerminalTheme
+    {
+        // Code snippet from "https://stackoverflow.com/questions/3369993/how-to-set-a-console-application-window-to-be-the-top-most-window-c"
+        // Used to keep console on top.
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int x,
+            int y,
+            int cx,
+            int cy,
+            int uFlags);
+
+        private const int HWND_TOPMOST = -1;
+        private const int SWP_NOMOVE = 0x0002;
+        private const int SWP_NOSIZE = 0x0001;
+
+        // Used to set opacity of terminal
+        [DllImport("user32.dll")]
+        static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+        // Code snippet from https://www.pinvoke.net/default.aspx/Structures.COLORREF
+        // For conversion from COLORREF to uint
+        private static uint MakeCOLORREF(byte r, byte g, byte b)
+        {
+            return (((uint)r) | (((uint)g) << 8) | (((uint)b) << 16));
+        }
+
+        public static void ApplyTheme()
+        {
+            // Keep window on top
+            IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
+
+            SetWindowPos(hWnd,
+                new IntPtr(HWND_TOPMOST),
+                0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE);
+
+            // Set opacity
+            SetLayeredWindowAttributes(hWnd,
+                MakeCOLORREF(0, 0, 0),
+                210, // Opacity number (0-255)
+                0x00000002
+                );
+        }
+    }
+}
