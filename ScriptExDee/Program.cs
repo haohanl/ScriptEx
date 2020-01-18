99422cd11768a200f1c7d1dc6381f101f36e3083
Haohan Liu <the1nonlypanda@gmail.com>
2020-01-19 01:10:42 +1100

    Update version

--- ScriptExDee/Program.cs
+++ ScriptExDee/Program.cs
@@ -21,7 +21,7 @@ namespace ScriptExDee
     class Program
     {
         // Program information
-        public static string Version = "20.01.10";
+        public static string Version = "20.01.19";
         public static string Title = "ScriptExDee";
         public static string Quote = Quotes.GetQuote();
 
