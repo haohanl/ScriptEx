f5414fa114ed17c8c7e27898f2b9196d95a5f0f4
Haohan Liu <the1nonlypanda@gmail.com>
2020-02-08 11:49:04 +1100

    Bump Assembly Version

--- ScriptExDee II/Properties/AssemblyInfo.cs
+++ ScriptExDee II/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ScriptExDee II")]
-[assembly: AssemblyCopyright("Copyright © Haohan Liu 2020")]
+[assembly: AssemblyCopyright("Haohan Liu © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("20.02.08")]
+[assembly: AssemblyFileVersion("20.02.08")]
